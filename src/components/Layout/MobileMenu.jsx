3a938ae9cf4aeb4345a74d3f0f30e990850cd89e
Topiia <devlop426@gmail.com>
2025-11-21 17:13:18 +0530

    Update spacing in MobileMenu component

--- src/components/Layout/MobileMenu.jsx
+++ src/components/Layout/MobileMenu.jsx
@@ -121,7 +121,7 @@ const MobileMenu = ({ isOpen, onClose, navigation }) => {
                       </Button>
                     </div>
                   ) : (
-                    <div className="flex flex-col space-y-6">
+                    <div className="space-y-8">
                       <Link to="/login" onClick={onClose}>
                         <Button variant="outline" fullWidth>
                           Login
